7f55390faa70b02dde8151f3344d7b8a455f293b
Gaurav <gaurav.adhikari36@gmail.com>
2022-06-16 18:54:55 +0545

    feat(mozio-utilities) fixed utility files

--- mozioapi/configurations/exceptions/mozio_core_exception.py
+++ mozioapi/configurations/exceptions/mozio_core_exception.py
@@ -1,10 +1,10 @@
 
 from rest_framework.exceptions import APIException
 from mozioapi.configurations.utilities.http_codes import BAD_REQUEST
-from ..utilities.api_response import BlogResponse
+from ..utilities.api_response import MozioResponse
 
 
-class BlogError(APIException):
+class MozioError(APIException):
     """Define the Api response error constants here.
     We should never alter the value of these constants
     from outside the scope of this class.
@@ -24,17 +24,17 @@ class BlogError(APIException):
         code=None,
     ):
         http_status = (
-            BlogError.status_code if (http_status is None) else http_status
+            MozioError.status_code if (http_status is None) else http_status
         )
 
-        code = BlogError.code if code is None else code
+        code = MozioError.code if code is None else code
 
         success = False
-        message = BlogError.message if message is None else message
-        data = BlogError.data if data is None else data
+        message = MozioError.message if message is None else message
+        data = MozioError.data if data is None else data
 
-        super(BlogError, self).__init__(
-            detail=BlogResponse(
+        super(MozioError, self).__init__(
+            detail=MozioResponse(
                 http_status=http_status,
                 message=message,
                 data=data,
